5d36e180af97ea30bab9a222bc7c02b1dba21f50
Elton Souza <eltonricsouza@hotmail.com.br>
2020-12-03 21:55:32 -0300

    Testes de unidade, iniciais.

---
+++ 1 - Testes de Unidade/Demo.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Demo;
+
+namespace Demo.Tests
+{
+  public class CalculatorTests
+  {
+    [Fact]
+    public void Calculator_Sum_ReturnSumValue()
+    {
+      // Arrange
+      var calculator = new Calculator();
+
+      // Act
+      var result = calculator.Sum(2, 2);
+
+      // Assert
+      Assert.Equal(4, result); // Com Equal é mais expressiva a mensagem caso dê erro, Exemplo: esperado 4, deu 6.
+      //Assert.True(result == 4); // Já o True só mostra True ou False.
+    }
+
+    [Theory]
+    [InlineData(1, 1, 2)]
+    [InlineData(3, 1, 4)]
+    [InlineData(5, 5, 10)]
+    [InlineData(100, 1, 101)]
+    [InlineData(50, -50, 0)]
+    // dá pra pegar os dados de fonte4s externas (Excel, banco de dados e etc.)
+    public void Calculator_Sum_ReturnCorrectlyValues(double v1, double v2, double total)
+    {
+      // Arrange
+      var calculator = new Calculator();
+
+      // Act
+      var result = calculator.Sum(v1, v2);
+
+      // Assert
+      Assert.Equal(total, result); // Com Equal é mais expressiva a mensagem caso dê erro, Exemplo: esperado 4, deu 6.
+    }
+  }
+}
---
+++ 1 - Testes de Unidade/Demo/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+  public class Calculator
+  {
+    public double Sum(double v1, double v2)
+    {
+      return v1 + v2;
+    }
+
+    public int Divisor(int v1, int v2)
+    {
+      return v1 / v2;
+    }
+  }
+}
