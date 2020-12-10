e0de85e51e65040b8bad9ff8a41a69cdd30cefd2
Elton Souza <eltonricsouza@hotmail.com.br>
2020-12-09 21:35:46 -0300

    Skipping testes.

---
+++ 1 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+  public class TesteNaoPassandoMotivoEspecifico
+  {
+    /* Lendo:
+     */
+    [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Esse teste está quebrando na versão 2.0. Então está sendo ignorado.")]
+    [Trait("Categoria", "Escapando dos testes")]
+    public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+    {
+      // Arrange
+      // Act
+      // Assert
+      Assert.True(false);
+    }
+  }
+}
